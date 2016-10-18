e1c2c1f3f88d6c3c229d00f9f22e36f53c3f93ee
左盐 <huabinglan@163.com>
2016-10-19 00:05:54 +0800

    fixbug

--- src/components/form/src/form.vue
+++ src/components/form/src/form.vue
@@ -11,12 +11,22 @@ export default {
       filedLength:0
     }
   },
-  created() {
-
-  },
+  created() {},
   computed: {},
-  mounted () {},
-  methods: {},
+  mounted () {
+    var a = this.$children;
+    console.log("children========");
+    console.log(a);
+    // for(let i=0,i<a.length;i<l;i++) {
+    //
+    // }
+    a[7].$children[0].$on("click", this.sub)
+  },
+  methods: {
+    sub:function() {
+      alert(1);
+    }
+  },
   components: {}
 }
 </script>
