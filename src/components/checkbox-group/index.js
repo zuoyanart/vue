4098b502ef6a4893105454ecc323d3f30ba9e8ce
左盐 <huabinglan@163.com>
2016-10-25 16:41:38 +0800

    build

--- src/components/checkbox-group/index.js
+++
@@ -1,7 +0,0 @@
-const PZCheckBoxGroup = require('./src/checkbox-group');
-
-PZCheckBoxGroup.install = function(Vue) {
-  Vue.component('pz-checkboxgroup', PZCheckBoxGroup);
-};
-
-module.exports = PZCheckBoxGroup;
--- src/components/form/index.js
+++
@@ -1,7 +0,0 @@
-const PZForm = require('./src/form');
-
-PZForm.install = function(Vue) {
-  Vue.component('pz-form', PZForm);
-};
-
-module.exports = PZForm;
