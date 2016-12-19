5ff60d83b8c3e9459019ff60e3fa0cfa94256b51
左盐 <huabinglan@163.com>
2016-12-19 18:15:21 +0800

    修正checkbox

--- src/components/checkbox/demo/App.vue
+++ src/components/checkbox/demo/App.vue
@@ -1,4 +1,6 @@
 <style lang="scss">
+@import "../../../assets/scss/pizza.scss";
+@import "../../../assets/site.scss";
    #app{
      margin:100px;
    }
@@ -7,10 +9,13 @@
 <template>
 
 <div id="app">
+  <h2 class="title">普通</h2>
   <pzcheckbox value="0" name="shop">京东</pzcheckbox>
   <pzcheckbox value="1" name="shop">淘宝</pzcheckbox>
   <pzcheckbox value="2" name="shop">天猫</pzcheckbox>
   <pzcheckbox value="3" name="shop" disabled="false">聚美优品</pzcheckbox>
+  <h2 class="title">绑定change事件</h2>
+  <pzcheckbox value="0" name="shop1" :change="change">京东</pzcheckbox>(打开控制台查看输出)
 </div>
 
 </template>
@@ -25,7 +30,12 @@ export default {
     components: {
         pzcheckbox,
     },
-    methods:{}
+    methods:{
+      change: function(value, checked) {
+          console.log("是否选中：" + checked);
+          console.log("checkbox的值：" + value);
+      }
+    }
 }
 
 </script>
