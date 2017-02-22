16083fb1c6deeec435895554a37b14230a508296
左盐 <huabinglan@163.com>
2017-02-22 18:47:17 +0800

    添加eslint

--- src/tools/tools.js
+++ src/tools/tools.js
@@ -12,7 +12,7 @@ let tools = (function() {
      */
     self.httpAgent = (url, method = 'get', data = '') => {
             method = method.toLowerCase();
-            if (method == "get" || method == "del") {
+            if (method === 'get' || method === 'del') {
                 return new Promise(function(resolve, reject) {
                     superagent[method].call(this, url).query(data).end(function(err, res) {
                         if (err || !res.ok) {
@@ -38,7 +38,7 @@ let tools = (function() {
          * @return {[type]}     [description]
          */
     self.getCharLen = function(str) { //获取字符串长度，区分中英文
-        return str.replace(/[^\x00-\xff]/g, "rr").length;
+        return str.replace(/[^\x00-\xff]/g, 'rr').length;
     }
     /**
      * 生成uuid
@@ -46,20 +46,20 @@ let tools = (function() {
      * @return {[type]} [description]
      */
     self.uuid = function() {
-        var chars = CHARS,
+        var chars = 'CHARS',
             uuid = new Array(36),
             rnd = 0,
             r;
         for (var i = 0; i < 36; i++) {
-            if (i == 8 || i == 13 || i == 18 || i == 23) {
+            if (i === 8 || i === 13 || i === 18 || i === 23) {
                 uuid[i] = '-';
-            } else if (i == 14) {
+            } else if (i === 14) {
                 uuid[i] = '4';
             } else {
                 if (rnd <= 0x02) rnd = 0x2000000 + (Math.random() * 0x1000000) | 0;
                 r = rnd & 0xf;
                 rnd = rnd >> 4;
-                uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+                uuid[i] = chars[(i === 19) ? (r & 0x3) | 0x8 : r];
             }
         }
         return uuid.join('');
